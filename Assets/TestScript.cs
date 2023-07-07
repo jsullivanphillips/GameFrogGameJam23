a4fa5a08e56b2c1ec967a586e25044167ea7962e
Gavin MacAulay <gavinmaca98@gmail.com>
2023-07-06 17:35:23 -0700

    First Commit WOo!

---
+++ Assets/TestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Did my test script work?
+    // should i eat spagetti or burrito for dinner?
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
