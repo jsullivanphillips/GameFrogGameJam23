5e304be26e95b093668161e896948795ed989d47
Jamie Sullivan-Phillips <47346975+jsullivanphillips@users.noreply.github.com>
2023-07-06 17:46:06 -0700

    mexican is goat

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,6 +7,8 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     // Did my test script work?
     // should i eat spagetti or burrito for dinner?
+    // I think burrito is very good for a hot day.
+    // mexican food is best
     void Start()
     {
         
