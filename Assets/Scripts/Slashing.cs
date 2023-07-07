e453b223a5cb6ff7258f4438724625a7620ccad8
Gavin MacAulay <gavinmaca98@gmail.com>
2023-07-07 15:15:57 -0700

    can shoot now

---
+++ Assets/Scripts/Slashing.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slashing : MonoBehaviour
+{
+
+    [SerializeField] Transform slashPoint;
+    [SerializeField] GameObject swordPrefab;
+
+    public float slashForce = 20f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+            GameObject sword = Instantiate(swordPrefab, slashPoint.position, slashPoint.rotation);
+            Rigidbody2D rb = sword.GetComponent<Rigidbody2D>();
+            rb.AddForce(slashPoint.up * slashForce, ForceMode2D.Impulse);
+    }
+}
--- Assets/Scripts/WerewolfMovement.cs
+++ Assets/Scripts/WerewolfMovement.cs
@@ -7,22 +7,30 @@ public class WerewolfMovement : MonoBehaviour
     Rigidbody2D body;
     float horizontal;
     float vertical;
+    public Camera cam;
 
-    public float runSpeed = 20.0f;
+    [SerializeField] private float runSpeed;
+    Vector2 mousePos;
 
     void Start ()
     {
-    body = GetComponent<Rigidbody2D>(); 
+        body = GetComponent<Rigidbody2D>(); 
     }
 
     void Update ()
     {
-    horizontal = Input.GetAxisRaw("Horizontal");
-    vertical = Input.GetAxisRaw("Vertical"); 
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical"); 
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {  
-    body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+
+        Vector2 lookDir = mousePos - body.position; 
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        body.rotation = angle;
     }
 }
